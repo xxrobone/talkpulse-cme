62825a31efe3e4fdd3495724e3dc57145c40311c
robonexx <robertwagar@gmail.com>
2023-12-05 11:10:19 +0100

    vite react for talk pulse client side

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h2>Clean project for talkpulse project</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
