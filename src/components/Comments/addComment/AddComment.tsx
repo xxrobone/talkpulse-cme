82d865e8353ffe55c88bfffb74c0f5ee8f370799
robonexx <robertwagar@gmail.com>
2023-12-21 13:26:38 +0100

    ... fix

--- src/components/Comments/addComment/AddComment.tsx
+++ src/components/Comments/addComment/AddComment.tsx
@@ -6,12 +6,6 @@ import { Post } from '../../../types/types';
 
 import styles from './AddComment.module.scss';
 
-interface AddCommentProps {
-  postId: string;
-  setAddAComment: Dispatch<SetStateAction<boolean>>;
-  setShowComments: Dispatch<SetStateAction<boolean>>;
-}
-
 export const action = async (args: ActionFunctionArgs) => {
   const { postId } = args.params;
 
@@ -39,23 +33,28 @@ export const action = async (args: ActionFunctionArgs) => {
   };
 };
 
+/* 
 const AddComment: React.FC<AddCommentProps> = ({
   postId,
   setAddAComment,
   setShowComments,
-}) => {
+})
+*/
+
+const AddComment = ({ postId }: { postId: string }) => {
   const fetcher = useFetcher({ key: 'comment-form-' + postId });
   const textRef = useRef<HTMLTextAreaElement>(null);
 
   if (fetcher.data && textRef.current) {
     textRef.current.value = '';
-    setAddAComment(false);
-    setShowComments(true);
   }
 
-  const handleCancelClick = () => {
+  /*   const handleCancelClick = () => {
     setAddAComment(false);
-  };
+
+
+    <button type='button' onClick={handleCancelClick}>
+  }; */
 
   return (
     <div className={styles['comment-form']}>
@@ -67,10 +66,7 @@ const AddComment: React.FC<AddCommentProps> = ({
         </div>
         <div className={styles['buttons-container']}>
           <button type='submit'>Post comment</button>{' '}
-          <button type='button' onClick={handleCancelClick}>
-            {' '}
-            Cancel
-          </button>
+          <button type='button'> Cancel</button>
         </div>
       </fetcher.Form>
     </div>
